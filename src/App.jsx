8093a92bfbd0985eaa7d195bcd62365a978f4da1
Kewal Kishan Gokuldas <kewal@Kewals-MBP.attlocal.net>
2023-09-05 12:35:14 -0700

    added responsiveness to pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Socials from "./pages/Socials";
 import { useRef } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter , Routes, Route} from "react-router-dom";
 import Projects from "./pages/Projects";
 import Events from "./pages/Events";
 import Showcase from "./pages/Showcase";
@@ -18,14 +18,12 @@ function App() {
     <Nav/>
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="socials" element={<Socials />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="events" element={<Events />} /> 
-          <Route path="showcase" element={<Showcase />} />
-          <Route path="xgames" element={<XGames />} /> 
-        </Route>
+          <Route path="/" index element={<Home />} />
+          <Route path="/socials" element={<Socials />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/events" element={<Events />} /> 
+          <Route path="/showcase" element={<Showcase />} />
+          <Route path="/xgames" element={<XGames />} /> 
       </Routes>
     </BrowserRouter>
     </div>
