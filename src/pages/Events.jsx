b20088bec985a586038f0394c3ac5ecf0c0554ac
Sayali Oak <sayalioak@Sayalis-MBP.attlocal.net>
2024-01-03 22:08:51 -0800

    responsiveness

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,4 +1,41 @@
 import React from 'react';
+import Event1I1 from '../assets/images/demo_screenshot (1).png';
+import Event2I1 from '../assets/images/1698310441329.jpeg';
+import Event2I2 from '../assets/images/1698310441802.jpeg';
+import Event2I3 from '../assets/images/1698310442197 (1).jpeg';
+import Event3I1 from '../assets/images/1701725619424.jpeg';
+import Event3I2 from '../assets/images/fall2023_1.jpeg';
+import Event3I3 from '../assets/images/fall2023_2 (1).jpeg';
+
+import Slider from "react-slick";
+// import "./Slideshow.css";  // We'll create this file next.
+// import "slick-carousel/slick/slick.css";
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black"}}
+      onClick={onClick}
+    />
+  );
+}
 
 function Events() {
 
@@ -8,41 +45,53 @@ function Events() {
       title: 'Intro to Aframe',
       link: 'https://github.com/kewalkishang/Aframe-Intro-ZombieSlayer',
       description: 'Make your very first WebVR game and learn the basics of Aframe, a popular WebVR framework.',
-      images: ['src/assets/images/demo_screenshot (1).png'], // Add your image URLs
+      images: [Event1I1], // Add your image URLs
     },
     {
       semester: 'Fall 2023',
       title: "X-Games '23",
       description: 'We set out with a vision to introduce students to the fascinating world of Extended Reality (XR) through an immersive gaming experience.',
-      images: ['src/assets/images/1698310441329.jpeg', 'src/assets/images/1698310441802.jpeg', 'src/assets/images/1698310442197 (1).jpeg'], // Add your image URLs
+      images: [Event2I1, Event2I2, Event2I3], // Add your image URLs
     },
     {
       semester: 'Fall 2023',
       title: "Transforming Portfolios into 3D",
       description: 'We delved into immersive web experiences, creating an interactive, gaze-based portfolio using the fundamentals of AR.js and A-Frame, offering participants a comprehensive exploration of building cutting-edge WebAR applications.',
-      images: ['src/assets/images/1701725619424.jpeg', 'src/assets/images/fall2023_1.jpeg', 'src/assets/images/fall2023_2 (1).jpeg'], // Add your image URLs
+      images: [Event3I1,Event3I2 ,Event3I3 ], // Add your image URLs
     },
   ];
 
 
   const renderCarousel = (images) => {
     const settings = {
-      // Customize carousel settings as needed
       dots: true,
-      infinite: false,
+      infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
     };
 
     return (
-      <div className="row__posters">
+      // <div className="row__posters">
+      //   {/* <div className='row__poster'> */}
+      // {images.map((image, index) => (
+      //   // <div>
+      //   // <img className='img_responsive' src={image} />
+      //   // </div>
+      //   <img style={{height: "180px", width : "100%"}} src={image}></img>
+      // ))}
+      // {/* </div> */}
+      // </div>
+
+      <Slider {...settings}>
       {images.map((image, index) => (
-        <div>
-        <img src={image} />
+        <div key={index}>
+          <img style={{height: "500px", width : "100%"}} src={image} alt={`Event ${index + 1}`} />
         </div>
       ))}
-      </div>
+    </Slider>
     );
   };
 
